91effae4eb51e86c69612c818d95b042a76a6c7b
Pravin G <pravgawle@gmail.com>
2024-04-22 12:21:50 +0530

    Remove private keys

--- res/components/ShopifyStoreConfig.tsx
+++ res/components/ShopifyStoreConfig.tsx
@@ -1,6 +1,4 @@
 export const shopifyAdminStoreUrl = 'https://e-comm-pos-dev-store.myshopify.com/admin/api/2024-01/graphql.json'
 export const shopifyStoreUrl = 'https://e-comm-pos-dev-store.myshopify.com/api/2024-01/graphql.json'
-export const storeFrontAccessToken = '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';
-export const storeFrontApiAccessToken = '060e4ed11c3706d6912c8ab3ebf895bb';
 
 
